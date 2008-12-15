56ebe5c90e8a3911cd6bcc34b6fddf19004a2a45
Henning Schmiedehausen <henning@apache.org>
2008-12-14 21:33:56 -0800

    add default binding for GadgetSpec

--- java/gadgets/src/main/java/org/apache/shindig/gadgets/DefaultGuiceModule.java
+++ java/gadgets/src/main/java/org/apache/shindig/gadgets/DefaultGuiceModule.java
@@ -23,7 +23,6 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.apache.shindig.common.guice.DefaultCommonModule;
 import org.apache.shindig.gadgets.http.BasicHttpFetcher;
 import org.apache.shindig.gadgets.http.DefaultHttpCache;
 import org.apache.shindig.gadgets.http.HttpCache;
@@ -41,6 +40,8 @@ import org.apache.shindig.gadgets.rewrite.ContentRewriterRegistry;
 import org.apache.shindig.gadgets.rewrite.DefaultContentRewriterRegistry;
 import org.apache.shindig.gadgets.rewrite.lexer.DefaultContentRewriter;
 import org.apache.shindig.gadgets.servlet.CajaContentRewriter;
+import org.apache.shindig.gadgets.spec.DefaultGadgetSpec;
+import org.apache.shindig.gadgets.spec.GadgetSpec;
 
 import com.google.common.collect.Lists;
 import com.google.inject.AbstractModule;
@@ -68,6 +69,7 @@ public class DefaultGuiceModule extends AbstractModule {
     bind(ContentRewriterRegistry.class).to(DefaultContentRewriterRegistry.class);
     bind(GadgetBlacklist.class).to(BasicGadgetBlacklist.class);
     bind(GadgetSpecFactory.class).to(DefaultGadgetSpecFactory.class);
+    bind(GadgetSpec.class).to(DefaultGadgetSpec.class);
     bind(LockedDomainService.class).to(HashLockedDomainService.class);
     bind(MessageBundleFactory.class).to(DefaultMessageBundleFactory.class);
     bind(UrlGenerator.class).to(DefaultUrlGenerator.class);
