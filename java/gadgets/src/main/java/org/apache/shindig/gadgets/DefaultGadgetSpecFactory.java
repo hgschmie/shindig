41e16706b9f346a8f42d57d442c0f6d12d4b7e8c
Henning Schmiedehausen <henning@apache.org>
2008-12-14 16:37:04 -0800

    factor out common code

--- java/gadgets/src/main/java/org/apache/shindig/gadgets/DefaultGadgetSpecFactory.java
+++ java/gadgets/src/main/java/org/apache/shindig/gadgets/DefaultGadgetSpecFactory.java
@@ -18,118 +18,29 @@
  */
 package org.apache.shindig.gadgets;
 
-import org.apache.shindig.common.cache.Cache;
 import org.apache.shindig.common.cache.CacheProvider;
-import org.apache.shindig.common.cache.SoftExpiringCache;
 import org.apache.shindig.common.uri.Uri;
-import org.apache.shindig.common.util.Check;
 import org.apache.shindig.gadgets.http.HttpFetcher;
-import org.apache.shindig.gadgets.http.HttpRequest;
-import org.apache.shindig.gadgets.http.HttpResponse;
 import org.apache.shindig.gadgets.spec.GadgetSpec;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.inject.name.Named;
 
-import java.net.URI;
-import java.util.logging.Logger;
-
 /**
  * Default implementation of a gadget spec factory.
  */
 @Singleton
-public class DefaultGadgetSpecFactory implements GadgetSpecFactory {
-  public static final String CACHE_NAME = "gadgetSpecs";
-  static final String RAW_GADGETSPEC_XML_PARAM_NAME = "rawxml";
-  static final Uri RAW_GADGET_URI = Uri.parse("http://localhost/raw.xml");
-  static final String ERROR_SPEC = "<Module><ModulePrefs title='Error'/><Content/></Module>";
-  static final String ERROR_KEY = "parse.exception";
-  static final Logger LOG = Logger.getLogger(DefaultGadgetSpecFactory.class.getName());
+public class DefaultGadgetSpecFactory extends AbstractGadgetSpecFactory implements GadgetSpecFactory {
 
-  private final HttpFetcher fetcher;
-  private final SoftExpiringCache<Uri, GadgetSpec> cache;
-  private final long refresh;
-
-  @Inject
-  public DefaultGadgetSpecFactory(HttpFetcher fetcher,
-                                  CacheProvider cacheProvider,
-                                  @Named("shindig.cache.xml.refreshInterval") long refresh) {
-    this.fetcher = fetcher;
-    Cache<Uri, GadgetSpec> baseCache = cacheProvider.createCache(CACHE_NAME);
-    this.cache = new SoftExpiringCache<Uri, GadgetSpec>(baseCache);
-    this.refresh = refresh;
-  }
-
-  public GadgetSpec getGadgetSpec(GadgetContext context) throws GadgetException {
-    String rawxml = context.getParameter(RAW_GADGETSPEC_XML_PARAM_NAME);
-    if (rawxml != null) {
-      // Set URI to a fixed, safe value (localhost), preventing a gadget rendered
-      // via raw XML (eg. via POST) to be rendered on a locked domain of any other
-      // gadget whose spec is hosted non-locally.
-      return new GadgetSpec(RAW_GADGET_URI, rawxml);
-    }
-    return getGadgetSpec(context.getUrl(), context.getIgnoreCache());
-  }
-
-  /**
-   * Retrieves a gadget specification from the cache or from the Internet.
-   */
-  public GadgetSpec getGadgetSpec(URI gadgetUri, boolean ignoreCache) throws GadgetException {
-    Uri uri = Uri.fromJavaUri(gadgetUri);
-    if (ignoreCache) {
-      return fetchObjectAndCache(uri, ignoreCache);
+    @Inject
+    public DefaultGadgetSpecFactory(final HttpFetcher fetcher,
+            final CacheProvider cacheProvider,
+            final@Named("shindig.cache.xml.refreshInterval") long refresh) {
+        super (fetcher, cacheProvider, refresh);
     }
 
-    SoftExpiringCache.CachedObject<GadgetSpec> cached = cache.getElement(uri);
-
-    GadgetSpec spec = null;
-    if (cached == null || cached.isExpired) {
-      try {
-        spec = fetchObjectAndCache(uri, ignoreCache);
-      } catch (GadgetException e) {
-        // Enforce negative caching.
-        if (cached != null) {
-          spec = cached.obj;
-          Check.notNull(spec);
-        } else {
-          // We create this dummy spec to avoid the cost of re-parsing when a remote site is out.
-          spec = new GadgetSpec(uri, ERROR_SPEC);
-          spec.setAttribute(ERROR_KEY, e);
-        }
-        LOG.info("GadgetSpec fetch failed for " + uri + " - using cached.");
-        cache.addElement(uri, spec, refresh);
-      }
-    } else {
-      spec = cached.obj;
+    protected GadgetSpec buildGadgetSpec(final Uri uri, final String xml) throws GadgetException {
+        return new GadgetSpec(uri, xml);
     }
-
-    GadgetException exception = (GadgetException) spec.getAttribute(ERROR_KEY);
-    if (exception != null) {
-      throw exception;
-    }
-    return spec;
-  }
-
-  /**
-   * Retrieves a gadget specification from the Internet, processes its views and
-   * adds it to the cache.
-   */
-  private GadgetSpec fetchObjectAndCache(Uri url, boolean ignoreCache) throws GadgetException {
-    HttpRequest request = new HttpRequest(url).setIgnoreCache(ignoreCache);
-    // Since we don't allow any variance in cache time, we should just force the cache time
-    // globally. This ensures propagation to shared caches when this is set.
-    request.setCacheTtl((int) (refresh / 1000));
-
-    HttpResponse response = fetcher.fetch(request);
-    if (response.getHttpStatusCode() != HttpResponse.SC_OK) {
-      throw new GadgetException(GadgetException.Code.FAILED_TO_RETRIEVE_CONTENT,
-                                "Unable to retrieve gadget xml. HTTP error " +
-                                response.getHttpStatusCode());
-    }
-
-    GadgetSpec spec = new GadgetSpec(url, response.getResponseAsString());
-    cache.addElement(url, spec, refresh);
-    return spec;
-  }
 }
