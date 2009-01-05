9031fb6325aede81f4e06477eee404707df38ebb
Henning Schmiedehausen <henning@apache.org>
2009-01-04 20:06:00 -0800

    desc *is* actually in the 0.8 gadgets xsd

--- java/gadgets/src/main/java/org/apache/shindig/gadgets/spec/LocaleMsg.java
+++ java/gadgets/src/main/java/org/apache/shindig/gadgets/spec/LocaleMsg.java
@@ -38,8 +38,6 @@ public class LocaleMsg extends SpecElement {
   public static final QName ELEMENT_NAME = new QName(SpecElement.OPENSOCIAL_NAMESPACE_URI, "msg");
 
   public static final String ATTR_NAME = "name";
-
-  /** Non 0.8 in gadgetspec, present in message bundle */
   public static final String ATTR_DESC = "desc";
 
   private StringBuilder text = new StringBuilder();
@@ -106,7 +104,6 @@ public class LocaleMsg extends SpecElement {
     return new EqualsBuilder()
                   .appendSuper(super.equals(other))
                   .append(getName(), rhs.getName())
-                  .append(getDesc(), rhs.getDesc())
                   .append(getText(), rhs.getText())
                   .isEquals();
   }
@@ -116,7 +113,6 @@ public class LocaleMsg extends SpecElement {
     return new HashCodeBuilder()
       .appendSuper(super.hashCode())
       .append(getName())
-      .append(getDesc())
       .append(getText())
       .toHashCode();
   }
@@ -125,6 +121,10 @@ public class LocaleMsg extends SpecElement {
 
     public Parser(final Uri base) {
       this(ELEMENT_NAME, base);
+    }
+    
+    public Parser(final QName parent, final QName child, final Uri base) {
+      this(buildChildName(parent, child, ELEMENT_NAME), base);
     }
 
     public Parser(final QName name, final Uri base) {
